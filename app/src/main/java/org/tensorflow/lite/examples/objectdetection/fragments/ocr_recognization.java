d1465f5a11358de45787d85cc13ee7d998f4b1c9
aman jain <am4193@srmist.edu.in>
2023-04-21 14:01:15 +0530

    finished project

---
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/fragments/ocr_recognization.java
@@ -0,0 +1,16 @@
+package org.tensorflow.lite.examples.objectdetection.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.tensorflow.lite.examples.objectdetection.R;
+
+public class ocr_recognization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ocr_recognization);
+
+    }
+}
